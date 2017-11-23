2f729aadd9b09ff0c3358c18176991bf04d07898
Jeppe Stærk <jeppe@staerk.io>
2017-11-23 15:34:48 +0100

    Firebase settings added

---
+++ app/src/main/java/au/ase/itsmap/e17/appproject/gruppe7/decide/SignInActivity.java
@@ -0,0 +1,13 @@
+package au.ase.itsmap.e17.appproject.gruppe7.decide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
